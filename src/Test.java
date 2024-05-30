dc9e83119bedaac1bf9082e1dde6a0846cb45df6
Sungchangyong <scychad1@gmail.com>
2024-05-30 17:17:11 +0900

    init

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,4 @@
-public class Test {
+//public class Test {
 
   /*public static void main(String[] args) {
       int a = 20, b = 30, c = 50;
@@ -32,23 +32,59 @@ public class Test {
 //        String str = "Hello World";
 //    }
 
+//    public static void main(String[] args){
+//        char c1 = 'A';
+//        char c2 = 65;
+//        char c3 = '\u0041';
+//
+//        char c4 = '가';
+//        char c5 = 44032;
+//        char c6 = '\uac00';
+//
+//        int uniCode = c1;
+//
+//        System.out.println(c1);
+//        System.out.println(c2);
+//        System.out.println(c3);
+//        System.out.println(c4);
+//        System.out.println(c5);
+//        System.out.println(c6);
+//        System.out.println(uniCode);
+//    }
+
+//    public static void main(String[] args){
+//        short sVal=10;
+//        short result=(short)(sVal+10);
+//        System.out.println(result);
+//    }
+//    public static void main(String[] args){
+//        float myFloat = 9.75f;
+//        float anotherFloat = myFloat * 2;
+//        System.out.println("Original float value: " + myFloat);
+//        System.out.println("Calculated float value: " + anotherFloat);
+//    }
+//public class Test implements TestInterface{
+//    public static void main(String[] args){
+//    int a=10;
+//
+//    }
+//
+//    @Override
+//    public void test() {
+//
+//    }
+//}
+
+public class Test {
+//    public static void main(String[] args){
+//        int[] arr={1, 2, 3, 4, 5};
+//        int[] arr2=new int[5];
+//
+//    }
     public static void main(String[] args){
-        char c1 = 'A';
-        char c2 = 65;
-        char c3 = '\u0041';
+        int a = 10;
+        int[] arr=new int[5];
 
-        char c4 = '가';
-        char c5 = 44032;
-        char c6 = '\uac00';
-
-        int uniCode = c1;
-
-        System.out.println(c1);
-        System.out.println(c2);
-        System.out.println(c3);
-        System.out.println(c4);
-        System.out.println(c5);
-        System.out.println(c6);
-        System.out.println(uniCode);
+        System.out.println(arr[0]);
     }
 }

01196a52411f6c819f908891f523f9b3d3254392
Sungchangyong <scychad1@gmail.com>
2024-05-30 13:56:32 +0900

    init

--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,54 @@
 public class Test {
 
-    /**
-     * @author 굿
-     * @apiNote 이것을 무엇을 하는 기능입니다.
-     * @since 2024.05.29
-     * @deprecated 사용하지 않는 기능입니다.
-     * @hidden
-     */
+  /*public static void main(String[] args) {
+      int a = 20, b = 30, c = 50;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("good");
-//        System.out.print(1);
+      System.out.println(a + b + c);
+    }
+  }*/
+
+//  public static void main(String[] args) {
+    // 정수형 자료형
+//      int a = 1;
+//    byte b = 2;
+//    long c = 3;
+//    short d = 4;
+    //실수형 자료형
+//    float e = 0.1f;
+//    double f = 0.2;
+
+//    public static void main(String[] args){
+//
+//        char a = 'a';
+//      }
+//    }
+
+//  public static void main(String[] args){
+//
+//    boolean isTrue = true;
+//    boolean isFalse = false;
+//  }
+//    public static void main(String[] args){
+//        String str = "Hello World";
+//    }
+
+    public static void main(String[] args){
+        char c1 = 'A';
+        char c2 = 65;
+        char c3 = '\u0041';
+
+        char c4 = '가';
+        char c5 = 44032;
+        char c6 = '\uac00';
+
+        int uniCode = c1;
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+        System.out.println(c5);
+        System.out.println(c6);
+        System.out.println(uniCode);
     }
 }

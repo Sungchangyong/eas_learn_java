677cb3f62cebedd8872de560cdd95096c4890f99
Sungchangyong <scychad1@gmail.com>
2024-05-30 09:34:36 +0900

    init

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+
+    /**
+     * @author 굿
+     * @apiNote 이것을 무엇을 하는 기능입니다.
+     * @since 2024.05.29
+     * @deprecated 사용하지 않는 기능입니다.
+     * @hidden
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("good");
+//        System.out.print(1);
+    }
+}
